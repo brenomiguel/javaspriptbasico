366b110d32745bcc486120a37a7320da7dbe169b
Breno Miguel <100931830+brenomiguel@users.noreply.github.com>
2023-09-12 12:45:16 -0300

    Add files via upload

--- aula_29_08_23/imc.js
+++ aula_29_08_23/imc.js
@@ -1,22 +1,22 @@
-function calculadora_imc(){
-    var altura = document.imcForm.altura.value;
-    var peso = document.imcForm. peso.value;
+function calcular_imc(){
+    var altura = document.imcForm.altura.value
+    var peso = document.imcForm.peso.value
 
-    var calculo = (peso/(altura*altura));
+    var calculo = (peso/(altura*altura))
 
-    if(calculo<18,5){
-        alert("vc tá abaixo do peso de acordo com indíce: " + calculo);
-    }
+    if(calculo<18.5){
+        alert("Você está magro com esse índice: " + calculo)
+    } 
     else if(calculo>=18.5 && calculo<24.9){
-        alert("vc tá com o peso normal de acordo com indíce: " + calculo);
+        alert("Você está normal com esse índice: " + calculo)
     }
     else if(calculo>=25 && calculo<29.9){
-        alert("vc tá sobre peso de acordo com indíce: " + calculo);
+        alert("Você está com sobre peso com esse índice: " + calculo)
     }
-    else if(calculo>=30 && calculo<39.9){
-        alert("vc tá com obesidade de acordo com indíce: " + calculo);
+    else if(calculo>= 30 && calculo<39.9){
+        alert("Você está com obesidade com esse índice: " + calculo)
     }
-    else if(calculo>40){
-        alert("vc tá com obesidade grave de acordo com indíce: " + calculo);
+    else if(calculo>=40){
+        alert("Você está com obesidade grave com esse índice: " + calculo)
     }
 }
--- aula_29_08_23/imc2.js
+++ aula_29_08_23/imc2.js
@@ -1,27 +1,25 @@
 function calcular_imc(){
+    event.preventDefault();
 
-    
-    var altura = document.imcForm.altura.value;
-    var peso = document.imcForm. peso.value;
+    var altura = document.imcForm.altura.value
+    var peso = document.imcForm.peso.value
 
-    var calculo = peso / (altura * altura);
+    var calculo = (peso/(altura*altura))
 
-    var resultado;
-
-    if(calculo<18,5){
-        resultado = "vc tá abaixo do peso de acordo com indíce: " + calculo;
-    }
+    if(calculo<18.5){
+        resultado = "Você está magro com esse índice: " + calculo
+    } 
     else if(calculo>=18.5 && calculo<24.9){
-        resultado = "vc tá com o peso normal de acordo com indíce: " + calculo;
+        resultado = "Você está normal com esse índice: " + calculo
     }
     else if(calculo>=25 && calculo<29.9){
-        resultado ="vc tá sobre peso de acordo com indíce: " + calculo;
+        resultado = "Você está com sobre peso com esse índice: " + calculo
     }
-    else if(calculo>=30 && calculo<39.9){
-        resultado = "vc tá com obesidade de acordo com indíce: " + calculo;
+    else if(calculo>= 30 && calculo<39.9){
+        resultado = "Você está com obesidade com esse índice: " + calculo
     }
-    else if(calculo>40){
-        resultado = "vc tá com obesidade grave de acordo com indíce: " + calculo;
+    else if(calculo>=40){
+        resultado = "Você está com obesidade grave com esse índice: " + calculo
     }
 
     document.getElementById('resul').innerHTML = resultado;
